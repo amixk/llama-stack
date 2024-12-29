8579c304bc7d89979b316b47638212dd3a12bf71
Amir M kazemi <cdsdxxd@gmail.com>
2024-12-29 09:40:08 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
-
+# forked to amixk
 from setuptools import find_packages, setup
 
 
